5d81553c5a61c057e09a9d4a6f9769a193d3c11e
Andy Hsiang <xenogt@gmail.com>
2016-11-01 17:58:42 -0400

    init detail page

--- app/scripts/controllers/wishdetails.js
+++ app/scripts/controllers/wishdetails.js
@@ -8,10 +8,25 @@
  * Controller of the wishlistApp
  */
 angular.module('wishlistApp')
-  .controller('WishdetailsCtrl', function () {
+  .controller('WishdetailsCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.wish=
+  		{
+  			requestor: 'Josh',
+  			name: 'Pumpkins',
+  			url: 'pumpkins.com',
+  			reason: 'I\'m hungry',
+  			score: 5,
+  			status: 'pending', // admin can change to {'approved','rejected','crowd'}
+  			liked: false,
+  			description: 'Pumpkins are so yummy!',
+  			imageUrl: 'https://s-media-cache-ak0.pinimg.com/736x/0a/a7/8a/0aa78af25e114836e1a42585fb7b09ed.jpg',
+  			comments: null, // to be filled out by admin upon status change
+  			cost: 14.99
+      }
   });
