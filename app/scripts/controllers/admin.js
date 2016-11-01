0f53ea95f78ff81c3b97d09b0488d2ce160490ed
Yosef Hoffman <yohoffman@gmail.com>
2016-11-01 16:15:19 -0400

    Add dummy data to admin page

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -8,10 +8,58 @@
  * Controller of the wishlistApp
  */
 angular.module('wishlistApp')
-  .controller('AdminCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('AdminCtrl', function ($scope) {
+	$scope.wishes = [
+  		{
+  			requestor: 'Josh',
+  			name: 'Pumpkins',
+  			url: 'pumpkins.com',
+  			reason: 'I\'m hungry',
+  			score: 5,
+  			status: 'pending', // admin can change to {'approved','rejected','crowd'}
+  			liked: false,
+  			description: 'Pumpkins are so yummy!',
+  			imageUrl: 'pumpkin.jpg',
+  			comments: null, // to be filled out by admin upon status change
+  			cost: 14.99
+  		},
+  		{
+  			requestor: 'Josh',
+  			name: 'Pumpkins',
+  			url: 'pumpkins.com',
+  			reason: 'I\'m hungry',
+  			score: 5,
+  			status: 'approved', // admin can change to {'approved','rejected','crowd'}
+  			liked: false,
+  			description: 'Pumpkins are so yummy!',
+  			imageUrl: 'pumpkin.jpg',
+  			comments: null, // to be filled out by admin upon status change
+  			cost: 14.99
+  		},
+  		{
+  			requestor: 'Josh',
+  			name: 'Pumpkins',
+  			url: 'pumpkins.com',
+  			reason: 'I\'m hungry',
+  			score: 5,
+  			status: 'rejected', // admin can change to {'approved','rejected','crowd'}
+  			liked: false,
+  			description: 'Pumpkins are so yummy!',
+  			imageUrl: 'pumpkin.jpg',
+  			comments: null, // to be filled out by admin upon status change
+  			cost: 14.99
+  		},
+  		{
+  			requestor: 'Josh',
+  			name: 'Pumpkins',
+  			url: 'pumpkins.com',
+  			reason: 'I\'m hungry',
+  			score: 5,
+  			status: 'crowd', // admin can change to {'approved','rejected','crowd'}
+  			liked: false,
+  			description: 'Pumpkins are so yummy!',
+  			imageUrl: 'pumpkin.jpg',
+  			comments: null, // to be filled out by admin upon status change
+  			cost: 14.99
+  		}
   });
