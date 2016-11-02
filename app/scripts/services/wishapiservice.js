92770b4cb832361c8f15a4f519f0d947b9900fe2
Julian Sy <js802v@att.com>
2016-11-02 14:53:52 -0400

    first draft wishlist

--- app/scripts/services/wishapiservice.js
+++ app/scripts/services/wishapiservice.js
@@ -8,6 +8,57 @@
  * Service in the wishlistApp.
  */
 angular.module('wishlistApp')
-  .service('wishApiService', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+  .factory('wishApiService', function () {
+      var _wishes = [];
+      var syncWishes = function() {
+      	_wishes = [
+  		    {
+      			requestor: 'Josh',
+    			name: 'Game Sphere',
+    			url: 'gamesphere.com',
+    			reason: 'no reason at all',
+    			score: 100,
+    			status: 'pending', // admin can change to {'approved','rejected','crowd'}
+    			liked: false,
+    			description: 'It\s spherical!',
+    			imageUrl: 'https://media.tenor.co/images/04631b0f77251af5e7dafe0b3a061724/raw',
+    			comments: null, // to be filled out by admin upon status change
+    			cost: 514.99
+    		},
+    		{
+    			requestor: 'Drake',
+    			name: 'Pumpkins',
+    			url: 'pumpkins.com',
+    			reason: 'I\'m hungry',
+    			score: 4,
+    			status: 'pending', // admin can change to {'approved','rejected','crowd'}
+    			liked: false,
+    			description: 'Pumpkins are so yummy!',
+    			imageUrl: 'pumpkin.jpg',
+    			comments: null, // to be filled out by admin upon status change
+    			cost: 14.99
+    		},
+    		{
+    			requestor: 'Josh',
+    			name: 'Pumpkins',
+    			url: 'pumpkins.com',
+    			reason: 'I\'m hungry',
+    			score: 5,
+    			status: 'pending', // admin can change to {'approved','rejected','crowd'}
+    			liked: false,
+    			description: 'Pumpkins are so yummy!',
+    			imageUrl: 'pumpkin.jpg',
+    			comments: null, // to be filled out by admin upon status change
+    			cost: 14.99
+    		}
+    	];
+        // TODO pull from backend
+      };
+
+      syncWishes();
+
+      return {
+          wishes:  _wishes,
+          syncWishes: syncWishes
+      };
   });
