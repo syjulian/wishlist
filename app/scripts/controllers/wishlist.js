f053045742b5c08536867309f82dce4de33f69cb
Julian Sy <js802v@att.com>
2016-11-03 16:44:18 -0400

    second draft wishlist

--- app/scripts/controllers/wishlist.js
+++ app/scripts/controllers/wishlist.js
@@ -8,16 +8,30 @@
  * Controller of the wishlistApp
  */
 angular.module('wishlistApp')
-  .controller('WishlistCtrl', function ($scope, wishApiService) {
+  .controller('WishlistCtrl', function ($scope, $location, 
+                                        wishApiService, selectedWishService) {
       $scope.orderOpts = [
         {label: 'By name', val: 'name'},
         {label: 'Highest Score', val: '-score'},
         {label: 'Lower Score', val: 'score '}
       ];
       $scope.selectedOrder = null;
+
       $scope.wishes = wishApiService.wishes;
 
       $scope.onSortClick = function(opt) {
           $scope.selectedOrder = opt;
       };
+
+      $scope.onLikeClick = function(wish) {
+          wish.liked = !wish.liked;
+          // TODO update backend
+      };
+
+      $scope.selectWish = function(wish) {
+        selectedWishService.setWish(wish);
+		selectedWishService.setBack('/wishlist');
+		$location.path('/wishDetails');
+	  };
+
   });
--- app/scripts/services/wishapiservice.js
+++ app/scripts/services/wishapiservice.js
@@ -26,17 +26,17 @@ angular.module('wishlistApp')
     			cost: 514.99
     		},
     		{
-	  			requestor: 'Josh',
-	  			name: 'Pumpkins',
-	  			url: 'pumpkins.com',
-	  			reason: 'I\'m hungry',
-	  			score: 5,
-	  			status: 'pending', // admin can change to {'approved','rejected','crowd'}
+	  			requestor: 'Tommy',
+	  			name: 'Rugrats Season 1 DVD',
+	  			url: 'https://www.amazon.com/Rugrats-Season-Disc-Melanie-Chartoff/dp/B00264H48O',
+	  			reason: 'Nostalgia feels',
+	  			score: 300,
+	  			status: 'pending',
 	  			liked: false,
-	  			description: 'Pumpkins are so yummy!',
-	  			imageUrl: 'http://healthyrise.com/wp-content/uploads/2016/09/Pumpkin-5.gif',
-	  			comments: null, // to be filled out by admin upon status change
-	  			cost: 14.99
+	  			description: 'What\'s better than a clean diapee and a fresh bottle? How about the first-ever, 25-episode, 4-disc set of the complete First Season of Rugrats! It\'s Tommy, Angelica, Chuckie and the rest of the gang, taking you on adventures from the ground up.',
+	  			imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51GaL5wlh8L.jpg',
+	  			comments: null, 
+	  			cost: 13.29
 	  		},
 	  		{
 	  			requestor: 'Josh',
@@ -82,10 +82,16 @@ angular.module('wishlistApp')
         // TODO pull from backend
       };
 
+      var updateWish = function(wish) {
+          console.log("sending update of " + wish);
+          //todo send POST/PUT to change wish
+      };
+
       syncWishes();
 
       return {
           wishes:  _wishes,
-          syncWishes: syncWishes
+          syncWishes: syncWishes,
+          updateWish: updateWish
       };
   });
