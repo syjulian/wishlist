643083ff370fbc3d98928a67553cea9158617325
Yosef Hoffman <yohoffman@gmail.com>
2016-11-03 16:59:08 -0400

    Prettify make a wish page

--- app/scripts/controllers/makewish.js
+++ app/scripts/controllers/makewish.js
@@ -7,18 +7,38 @@
  * # MakewishCtrl
  * Controller of the wishlistApp
  */
-angular.module('wishlistApp')
-  .controller('MakewishCtrl',['$scope', '$location', 'wishApiService', function ($scope, $location, wishApiService) {
-		
-		$scope.wish = null
+angular.module('wishlistApp').controller('MakewishCtrl', ['$scope', '$location', '$window', '$log', 'wishApiService',
+function($scope, $location, $window, $log, wishApiService) {
 
-		$scope.submit = function(){
-                        if($scope.wish){
-                        	$scope.wish.user = "DEFAULT USER"
-				$scope.wish.status = "new"
-				wishApiService.createWish($scope.wish)
-			} else {
-				alert("You've gotta give me something")
+	$scope.wish = null;
+	$scope.errors = [];
+
+	$scope.reasons = ['Improve productivity', 'Improve morale', 'Improve communication', 'Maintain existing service', 'Other'];
+	$scope.categories = ['Item', 'Event', 'Process', 'Other'];
+
+	$scope.submit = function() {
+		$scope.errors = [];
+		var valid = true;
+		if (!$scope.wish) {
+			valid = false;
+			$scope.errors.push('You\'ve gotta give me something!');
+		} else {
+			if ($scope.makeWishForm.$error.required) {
+				valid = false;
+				$scope.errors.push('Please fill out all required fields!');
 			}
-		};
-  }]);
+		}
+		if (valid) {
+			$scope.wish.user = 'DEFAULT USER';
+			$scope.wish.status = 'pending';
+			$log.info('creating wish: ', $scope.wish);
+			wishApiService.createWish($scope.wish).then(function successCallback(response) {
+				$log.info('Successfully created wish: ', response);
+			}, function errorCallback(response) {
+				$log.error('Error creating wish: ', response);
+			});
+		} else {
+			$window.scrollTo(0, 0);
+		}
+	};
+}]);
--- app/scripts/services/wishapiservice.js
+++ app/scripts/services/wishapiservice.js
@@ -7,99 +7,86 @@
  * # wishApiService
  * Service in the wishlistApp.
  */
-angular.module('wishlistApp')
-  .factory('wishApiService', function ($http) {
-      var _wishes = [];
-      var createWish = function(wish) {
-		console.log("creating wish")
-		console.log(wish)
-                $http({
-  			method: 'POST',
-  			url: '/createWish',
-                        data: wish
-		}).then(function successCallback(response) {
-    			alert("WE DID IT!")
-  		}, function errorCallback(response) {
-    			alert("oh no, there was an error")
-  		});
-      };
-      var syncWishes = function() {
-      	_wishes = [
-  		    {
-      			requestor: 'Josh',
-    			name: 'Game Sphere',
-    			url: 'gamesphere.com',
-    			reason: 'no reason at all',
-    			score: 100,
-    			status: 'pending', // admin can change to {'approved','rejected','crowd'}
-    			liked: false,
-    			description: 'It\s spherical!',
-    			imageUrl: 'https://media.tenor.co/images/04631b0f77251af5e7dafe0b3a061724/raw',
-    			comments: null, // to be filled out by admin upon status change
-    			cost: 514.99
-    		},
-    		{
-	  			requestor: 'Josh',
-	  			name: 'Pumpkins',
-	  			url: 'pumpkins.com',
-	  			reason: 'I\'m hungry',
-	  			score: 5,
-	  			status: 'pending', // admin can change to {'approved','rejected','crowd'}
-	  			liked: false,
-	  			description: 'Pumpkins are so yummy!',
-	  			imageUrl: 'http://healthyrise.com/wp-content/uploads/2016/09/Pumpkin-5.gif',
-	  			comments: null, // to be filled out by admin upon status change
-	  			cost: 14.99
-	  		},
-	  		{
-	  			requestor: 'Josh',
-	  			name: 'Pumpkins',
-	  			url: 'pumpkins.com',
-	  			reason: 'I\'m hungry',
-	  			score: 25,
-	  			status: 'approved', // admin can change to {'approved','rejected','crowd'}
-	  			liked: false,
-	  			description: 'Pumpkins are so yummy!',
-	  			imageUrl: 'http://healthyrise.com/wp-content/uploads/2016/09/Pumpkin-5.gif',
-	  			comments: null, // to be filled out by admin upon status change
-	  			cost: 14.99
-	  		},
-	  		{
-	  			requestor: 'Josh',
-	  			name: 'Pumpkins',
-	  			url: 'pumpkins.com',
-	  			reason: 'I\'m hungry',
-	  			score: 5,
-	  			status: 'rejected', // admin can change to {'approved','rejected','crowd'}
-	  			liked: false,
-	  			description: 'Pumpkins are so yummy!',
-	  			imageUrl: 'http://healthyrise.com/wp-content/uploads/2016/09/Pumpkin-5.gif',
-	  			comments: null, // to be filled out by admin upon status change
-	  			cost: 14.99
-	  		},
-	  		{
-	  			requestor: 'Josh',
-	  			name: 'Pumpkins',
-	  			url: 'pumpkins.com',
-	  			reason: 'I\'m hungry',
-	  			score: 5,
-	  			status: 'crowd', // admin can change to {'approved','rejected','crowd'}
-	  			liked: false,
-	  			description: 'Pumpkins are so yummy!',
-	  			imageUrl: 'http://healthyrise.com/wp-content/uploads/2016/09/Pumpkin-5.gif',
-	  			comments: null, // to be filled out by admin upon status change
-	  			cost: 14.99,
-	  			raised: 10.99
-	  		}
-    	];
-        // TODO pull from backend
-      };
+angular.module('wishlistApp').factory('wishApiService', function($http) {
+	var _wishes = [];
+	var createWish = function(wish) {
+		return $http({
+			method : 'POST',
+			url : '/createWish',
+			data : wish
+		});
+	};
+	var syncWishes = function() {
+		_wishes = [{
+			requestor : 'Josh',
+			name : 'Game Sphere',
+			url : 'gamesphere.com',
+			reason : 'no reason at all',
+			score : 100,
+			status : 'pending', // admin can change to {'approved','rejected','crowd'}
+			liked : false,
+			description : 'It\s spherical!',
+			imageUrl : 'https://media.tenor.co/images/04631b0f77251af5e7dafe0b3a061724/raw',
+			comments : null, // to be filled out by admin upon status change
+			cost : 514.99
+		}, {
+			requestor : 'Josh',
+			name : 'Pumpkins',
+			url : 'pumpkins.com',
+			reason : 'I\'m hungry',
+			score : 5,
+			status : 'pending', // admin can change to {'approved','rejected','crowd'}
+			liked : false,
+			description : 'Pumpkins are so yummy!',
+			imageUrl : 'http://healthyrise.com/wp-content/uploads/2016/09/Pumpkin-5.gif',
+			comments : null, // to be filled out by admin upon status change
+			cost : 14.99
+		}, {
+			requestor : 'Josh',
+			name : 'Pumpkins',
+			url : 'pumpkins.com',
+			reason : 'I\'m hungry',
+			score : 25,
+			status : 'approved', // admin can change to {'approved','rejected','crowd'}
+			liked : false,
+			description : 'Pumpkins are so yummy!',
+			imageUrl : 'http://healthyrise.com/wp-content/uploads/2016/09/Pumpkin-5.gif',
+			comments : null, // to be filled out by admin upon status change
+			cost : 14.99
+		}, {
+			requestor : 'Josh',
+			name : 'Pumpkins',
+			url : 'pumpkins.com',
+			reason : 'I\'m hungry',
+			score : 5,
+			status : 'rejected', // admin can change to {'approved','rejected','crowd'}
+			liked : false,
+			description : 'Pumpkins are so yummy!',
+			imageUrl : 'http://healthyrise.com/wp-content/uploads/2016/09/Pumpkin-5.gif',
+			comments : null, // to be filled out by admin upon status change
+			cost : 14.99
+		}, {
+			requestor : 'Josh',
+			name : 'Pumpkins',
+			url : 'pumpkins.com',
+			reason : 'I\'m hungry',
+			score : 5,
+			status : 'crowd', // admin can change to {'approved','rejected','crowd'}
+			liked : false,
+			description : 'Pumpkins are so yummy!',
+			imageUrl : 'http://healthyrise.com/wp-content/uploads/2016/09/Pumpkin-5.gif',
+			comments : null, // to be filled out by admin upon status change
+			cost : 14.99,
+			raised : 10.99
+		}];
+		// TODO pull from backend
+	};
 
-      syncWishes();
+	syncWishes();
 
-      return {
-          wishes:  _wishes,
-          createWish: createWish,
-          syncWishes: syncWishes
-      };
-  });
+	return {
+		wishes : _wishes,
+		createWish : createWish,
+		syncWishes : syncWishes
+	};
+});
