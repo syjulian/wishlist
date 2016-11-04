cf04e23264d487d1c9d7c7aa37a0153b67705ec2
Yosef Hoffman <yohoffman@gmail.com>
2016-11-04 13:42:19 -0400

    Format raised amount to 2 decimals and fix jshint errors

--- app/scripts/controllers/wishdetails.js
+++ app/scripts/controllers/wishdetails.js
@@ -7,67 +7,72 @@
  * # WishdetailsCtrl
  * Controller of the wishlistApp
  */
-angular.module('wishlistApp')
-  .controller('WishdetailsCtrl', ['$scope', '$location', 'wishApiService', 'selectedWishService', function ($scope, $location, wishApiService, selectedWishService) {
+angular.module('wishlistApp').controller('WishdetailsCtrl', ['$scope', '$location', 'wishApiService', 'selectedWishService',
+function($scope, $location, wishApiService, selectedWishService) {
 
-		$scope.wish = selectedWishService.getWish();
-		var calledFrom = selectedWishService.getBack();
-		if(calledFrom == '/admin') $scope.isAdmin = true;
-		else if (calledFrom == '/wishlist') $scope.isAdmin = false;
-		$scope.isCrowd = ($scope.wish.status == 'crowd');
+	$scope.wish = selectedWishService.getWish();
+	var calledFrom = selectedWishService.getBack();
+	if (calledFrom === '/admin') {
+		$scope.isAdmin = true;
+	} else if (calledFrom === '/wishlist') {
+		$scope.isAdmin = false;
+	}
+	$scope.isCrowd = ($scope.wish.status === 'crowd');
 
-		$scope.goBack = function(){
-			$location.path(selectedWishService.getBack());
-		};
+	$scope.goBack = function() {
+		$location.path(selectedWishService.getBack());
+	};
 
-		$scope.clickedHeart = function() {
+	$scope.clickedHeart = function() {
 
-			var liked = $scope.wish.liked;
-			$scope.wish.liked = !$scope.wish.liked;
-			if (liked) {
-				$scope.wish.score -=1;
-			} else {
-				$scope.wish.score +=1;
-			}
-		};
-
-		$scope.donateMoney = function(){
-			console.log($scope.wish);
-			$scope.donation = $scope.wish.cost - $scope.wish.raised;
-			if ($scope.wish.cost >= $scope.wish.raised)
-				$scope.showInput = true;
-			else $scope.showInput = false;
+		var liked = $scope.wish.liked;
+		$scope.wish.liked = !$scope.wish.liked;
+		if (liked) {
+			$scope.wish.score -= 1;
+		} else {
+			$scope.wish.score += 1;
 		}
+	};
 
-		$scope.cancel = function(){
+	$scope.donateMoney = function() {
+		console.log($scope.wish);
+		$scope.donation = $scope.wish.cost - $scope.wish.raised;
+		if ($scope.wish.cost >= $scope.wish.raised) {
+			$scope.showInput = true;
+		} else {
+			$scope.showInput = false;
+		}
+	};
+
+	$scope.cancel = function() {
+		$scope.showInput = false;
+		$scope.donation = 0;
+	};
+
+	$scope.submit = function() {
+		if (isNaN($scope.donation)) {
+			$scope.donation = 0;
+			return;
+		}
+		if ($scope.donation > 0) {
+			$scope.wish.donated = true;
+			$scope.wish.raised = (parseInt(($scope.wish.raised + $scope.donation + 0.001 ) * 100) + 0.001) / 100.0;
 			$scope.showInput = false;
 			$scope.donation = 0;
 		}
+	};
 
-		$scope.submit = function(){
-			if (isNaN($scope.donation)) {
-				$scope.donation = 0;
-				return;
-			}
-			if($scope.donation > 0) {
-				$scope.wish.donated = true;
-				$scope.wish.raised = (parseInt(($scope.wish.raised + $scope.donation+0.001 )*100)+0.001)/100.0;
-				$scope.showInput=false;
-				$scope.donation = 0;
-			}
-		}
-
-		$scope.toApprove = function(){
-			$scope.wish.status = 'approved';
-			$scope.goBack();
-		}
-		$scope.toReject = function(){
-			$scope.wish.status = 'rejected';
-			$scope.goBack();
-		}
-		$scope.toCrowd = function(){
-			$scope.wish.status = 'crowd';
-			$scope.wish.raised = 0;
-			$scope.goBack();
-		}
-  }]);
+	$scope.toApprove = function() {
+		$scope.wish.status = 'approved';
+		$scope.goBack();
+	};
+	$scope.toReject = function() {
+		$scope.wish.status = 'rejected';
+		$scope.goBack();
+	};
+	$scope.toCrowd = function() {
+		$scope.wish.status = 'crowd';
+		$scope.wish.raised = 0;
+		$scope.goBack();
+	};
+}]);
