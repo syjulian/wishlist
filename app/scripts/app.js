25fd4f5eb82fca3728f7c414f7870e5781864ff0
Felipe Monteiro <fm577c@att.com>
2016-11-04 17:14:02 -0400

    Fixed login bug.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -54,7 +54,7 @@ angular
   })
   .run(function($rootScope, $location, localStorage) {
     $rootScope.isLoggedIn = false;
-    $rootScope.server = 'http://localhost:3000/'; //'http://wishlist-backend.herokuapp.com/';
+    $rootScope.server = 'http://wishlist-backend.herokuapp.com/';
 
     $rootScope.$watch(function() {
       return localStorage.get('user');
